29272107707e3c4873bfa5fd0861691dd8c3c586
srini161 <sinuravi2001@gmail.com>
2023-01-30 17:45:39 +0530

    hello

--- example/Program.vb
+++ example/Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("My name is Srinivas R")
+        Console.WriteLine("Iam from Hosur")
     End Sub
 End Module
