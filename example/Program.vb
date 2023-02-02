e3fde21ff7e4861d36c59a6c97737d3d8d5d8c76
srini161 <sinuravi2001@gmail.com>
2023-02-02 11:56:01 +0530

    comp

--- example/Program.vb
+++ example/Program.vb
@@ -5,12 +5,14 @@ Module Program
 
     Public Sub Main()
 
-            Dim Acceptdetails As Acceptdetails = New Acceptdetails
-            Acceptdetails.AcceptDetails1()
-        Acceptdetails.DisplayDetails()
-        Console.WriteLine("Event:" + vbTab + "Tennis Match")
-        Console.WriteLine("Venue:" + vbTab + "Star Sports Complex")
-        Console.WriteLine("Time:" + vbTab + "4:00 p.m to 6:00 pm")
+        'Dim Acceptdetails As Acceptdetails = New Acceptdetails
+        'Acceptdetails.AcceptDetails1()
+        'Acceptdetails.DisplayDetails()
+        'Console.WriteLine("Event:" + vbTab + "Tennis Match")
+        'Console.WriteLine("Venue:" + vbTab + "Star Sports Complex")
+        'Console.WriteLine("Time:" + vbTab + "4:00 p.m to 6:00 pm")
+        Dim AddInt As operators = New operators
+        AddInt.ArthemeticOperations()
 
     End Sub
 
---
+++ example/operators.vb
@@ -0,0 +1,48 @@
+﻿Public Class operators
+    Dim a As Int32
+    Dim b As Int32
+    Dim c As Int32
+
+    Public Sub ArthemeticOperations()
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+
+        c = a + b
+        Console.WriteLine("After add the result is: {0}", c)
+
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+
+        c = a - b
+        Console.WriteLine("After sub the result is: {0}", c)
+
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+
+        c = a * b
+        Console.WriteLine("After multiply the result is: {0}", c)
+
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+
+        c = a / b
+        Console.WriteLine("After Divide the result is: {0}", c)
+
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+
+        c = a Mod b
+        Console.WriteLine("After Mod the result is: {0}", c)
+
+    End Sub
+End Class
