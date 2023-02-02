0f54491520f2698aaea1b9e15094b3ad763db60b
srini161 <sinuravi2001@gmail.com>
2023-02-02 10:32:14 +0530

    kl

--- example/Program.vb
+++ example/Program.vb
@@ -10,7 +10,7 @@ Module Program
         Acceptdetails.DisplayDetails()
         Console.WriteLine("Event:" + vbTab + "Tennis Match")
         Console.WriteLine("Venue:" + vbTab + "Star Sports Complex")
-        Console.WriteLine("Time:" + vbTab + "4:00 p.m to 6;00 pm")
+        Console.WriteLine("Time:" + vbTab + "4:00 p.m to 6:00 pm")
 
     End Sub
 
